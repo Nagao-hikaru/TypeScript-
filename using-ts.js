972c74e36aff624d4b3b8cb715dc5e004e989a7c
Nagao-hikaru <koyanagi29sushi@gmail.com>
2023-03-14 23:14:39 +0900

    lite-serverの追加

--- using-ts.js
+++
@@ -1,8 +0,0 @@
-var button = document.querySelector('button');
-var input = document.getElementById('num');
-function add(num1, num2) {
-    return num1;
-}
-button === null || button === void 0 ? void 0 : button.addEventListener('click', function () {
-    console.log(add(input.value, 'ddd'));
-});
