831aea77ccde6201d085f1994f375b99ff2f72c5
Nagao-hikaru <koyanagi29sushi@gmail.com>
2023-03-14 23:00:18 +0900

    Tsファイルのコンパイル

---
+++ using-ts.js
@@ -0,0 +1,8 @@
+var button = document.querySelector('button');
+var input = document.getElementById('num');
+function add(num1, num2) {
+    return num1;
+}
+button === null || button === void 0 ? void 0 : button.addEventListener('click', function () {
+    console.log(add(input.value, 'ddd'));
+});
