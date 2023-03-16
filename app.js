eac786ce81a5c2134015a0e9cf2f606e0376fa18
Nagao-hikaru <koyanagi29sushi@gmail.com>
2023-03-17 07:31:01 +0900

    number型の学習

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log('kkk');
+function add(n1, n2) {
+    return n1 + n2;
+}
+var n1 = 5;
+var n2 = 2.5;
+var number = add(n1, n2);
+console.log(number);
--- app.ts
+++ app.ts
@@ -1,1 +1,11 @@
-console.log('kkk')
+function aa(n1: number, n2:number) {
+  return n1 + n2
+}
+
+const rr = '6'
+
+const n2 = 2.5
+
+const number = aa(rr, n2)
+
+console.log(number)
