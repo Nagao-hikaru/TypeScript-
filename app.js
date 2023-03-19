36548b6619ba14196df6de904b82b5dc1c592fbe
Nagao-hikaru <koyanagi29sushi@gmail.com>
2023-03-19 14:28:27 +0900

    enum型

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
-var person;
-person = {
+// let person: {
+//   name: string,
+//   age: number,
+//   sports: string[]
+//   role: [number, string]
+//   //tuple型
+// }
+// person = {
+//   name: 'aaa',
+//   age: 22,
+//   sports: ['soccer', 'baseball', ],
+//   role: [2, 'nagao '],
+// }
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ"] = 1] = "READ";
+    Role[Role["AAA"] = 2] = "AAA";
+})(Role || (Role = {}));
+var person = {
     name: 'aaa',
     age: 22,
-    sports: ['soccer', 'baseball',]
+    sports: ['soccer', 'baseball',],
+    role: Role.AAA
 };
 for (var _i = 0, _a = person.sports; _i < _a.length; _i++) {
     var sport = _a[_i];
     console.log(sport);
-    debugger;
 }
-console.error(person);
+if (person.role == Role.AAA) {
+    console.log('dfd');
+}
--- app.ts
+++ app.ts
@@ -1,20 +1,33 @@
-let person: {
-  name: string,
-  age: number,
-  sports: string[]
-  role: [number, string]
-  //tuple型
+// let person: {
+//   name: string,
+//   age: number,
+//   sports: string[]
+//   role: [number, string]
+//   //tuple型
+// }
+
+// person = {
+//   name: 'aaa',
+//   age: 22,
+//   sports: ['soccer', 'baseball', ],
+//   role: [2, 'nagao '],
+// }
+
+enum Role {
+  ADMIN, READ, AAA
 }
 
-person = {
+const person = {
   name: 'aaa',
   age: 22,
   sports: ['soccer', 'baseball', ],
-  role: [2, 'nagao '],
+  role: Role.AAA,
 }
 
 for (const sport of person.sports) {
   console.log(sport)
 }
 
-console.error(person )
+if (person.role == Role.AAA) {
+  console.log('dfd')
+}
