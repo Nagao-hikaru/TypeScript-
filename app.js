c42e16015773421b59d5d365223c38476244f5be
Nagao-hikaru <koyanagi29sushi@gmail.com>
2023-03-20 07:39:47 +0900

    関数の型

--- app.js
+++ app.js
@@ -4,4 +4,6 @@ function add(n1, n2) {
 function printResulet(n1) {
     console.log(n1);
 }
+var aaa;
+aaa = add;
 printResulet(add(5, 10));
--- app.ts
+++ app.ts
@@ -6,4 +6,7 @@ function printResulet(n1: number): void {
   console.log(n1)
 }
 
+let aaa: (type1: number, type2: number) => number
+
+aaa = add
 printResulet(add(5, 10))
