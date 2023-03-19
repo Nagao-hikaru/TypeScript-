1e7ba6b962e2eb8af554c7ed8a29daa838c60ab4
Nagao-hikaru <koyanagi29sushi@gmail.com>
2023-03-19 19:49:10 +0900

    型エイリアス

--- app.ts
+++ app.ts
@@ -1,22 +1,7 @@
-// function combine(input1: number | string, input2: number | string) {
-//   let result;
-//   if (typeof input1 === 'number' && typeof input2 === 'number') {
-//     result = input1 + input2;
-//   } else {
-//     result = input1.toString() + input2.toString();
-//   }
-//   return result;
-// }
+type combinable = number | string
 
-// const combinedAges = combine(30, 26);
-// console.log(combinedAges);
-
-// const combinedNames = combine('Max', 'Anna');
-// console.log(combinedNames);
-
-function combine(input1: number | string, input2: number | string) {
-  const result = input1 + input2
-  return result
+function combine(input1: combinable, input2: combinable) {
+  return input1
 }
 
 const combinedAges = combine(30, 26) 
