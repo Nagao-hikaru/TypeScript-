6e25f0b41606fb48673ab3d563fce78bc35a2cc7
Nagao-hikaru <koyanagi29sushi@gmail.com>
2023-03-21 12:54:49 +0900

    callback関数の指定

--- app.ts
+++ app.ts
@@ -10,3 +10,8 @@ let aaa: (type1: number, type2: number) => number
 
 aaa = add
 printResulet(add(5, 10))
+
+function addAndResult(n1: number, n2: number, cb: (num: number) => void) {
+ const result = n1 + n2
+ cb(result)
+}
