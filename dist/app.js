6cf05123a2bb440f7b374092054f8944074e0422
Nagao-hikaru <koyanagi29sushi@gmail.com>
2023-03-26 15:12:45 +0900

    スプレッド演算子

--- dist/app.js
+++ dist/app.js
@@ -4,4 +4,7 @@ var addNow = function (a, b) {
     return a + b;
 };
 console.log(addNow(1, 2));
+var hobbies = ['sports', 'cooking'];
+hobbies.push.apply(hobbies, hobbies);
+console.log(hobbies);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -3,3 +3,8 @@ const addNow = (a: number, b: number = 22) => {
 }
 
 console.log(addNow(1, 2))
+
+const hobbies = ['sports', 'cooking']
+
+hobbies.push(...hobbies)
+console.log(hobbies)
