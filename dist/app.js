b9c3cdfacdd274603cd77f25aeb83639be4e84b4
Nagao-hikaru <koyanagi29sushi@gmail.com>
2023-03-28 22:06:18 +0900

    レストパラメーターの学習

--- dist/app.js
+++ dist/app.js
@@ -8,4 +8,14 @@ var hobbies = ['sports', 'cooking'];
 hobbies.push.apply(hobbies, hobbies);
 // スプレッド演算子
 console.log(hobbies);
+var uu = function () {
+    var numbers = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        numbers[_i] = arguments[_i];
+    }
+    return numbers.reduce(function (result, value) {
+        return result + value;
+    }, 0);
+};
+console.log(uu(1, 2, 3, 4));
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -9,3 +9,11 @@ const hobbies = ['sports', 'cooking']
 hobbies.push(...hobbies)
 // スプレッド演算子
 console.log(hobbies)
+
+const uu = (...numbers: number[]) => {
+  return numbers.reduce((result, value) => {
+    return result + value
+  }, 0);
+}
+
+console.log(uu(1, 2, 3, 4))
