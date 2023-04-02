5ad82f3094954c24a5672e7ae95616fa2d32c930
Nagao-hikaru <koyanagi29sushi@gmail.com>
2023-04-03 07:35:03 +0900

    プロパティの参照

--- dist/app.js
+++ dist/app.js
@@ -1,11 +1,17 @@
 "use strict";
 class Department {
     constructor(n) {
+        this.emp = [];
         this.name = n;
     }
     describe() {
+        console.log('department' + this.name);
+    }
+    addEmp(employe) {
+        this.emp.push(employe);
+        console.log(this.emp);
     }
 }
 const accounting = new Department('nagao');
-console.log(accounting);
+console.log(accounting.addEmp('uu'));
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,23 @@
 class Department {
   name: string
+  private emp: string[] = []
 
   constructor(n: string) {
     this.name = n
   }
 
-  describe() {
-    
+  describe(this: Department) {
+    console.log('department' + this.name)
+  }
+
+  addEmp(employe: string) {
+    this.emp.push(employe)
+    console.log(this.emp)
   }
 }
 
 const accounting = new Department('nagao')
 
-console.log(accounting)
+
+
+console.log(accounting.addEmp('uu'))
