368bb366c2f62d10a6c9975ddfa932ec8763bd0c
Nagao-hikaru <koyanagi29sushi@gmail.com>
2023-04-18 21:13:17 +0900

    interfaceの実装

--- dist/app.js
+++ dist/app.js
@@ -1,12 +1,18 @@
 "use strict";
 let user;
-user = {
-    name: 'man',
-    age: 30,
-    greet(phase) {
-        console.log(phase);
-    }
-};
+// user = {
+//   name: 'man',
+//   age: 30,
+//   greet(phase: string) {
+//     console.log(phase)
+//   }
+// }
 class Person {
+    constructor(n) {
+        this.age = n;
+    }
+    greet(ja) {
+        console.log(ja);
+    }
 }
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -6,14 +6,23 @@ interface Greetable {
 
 let user: Greetable;
 
-user = {
-  name: 'man',
-  age: 30,
-  greet(phase: string) {
-    console.log(phase)
-  }
+// user = {
+//   name: 'man',
+//   age: 30,
+//   greet(phase: string) {
+//     console.log(phase)
+//   }
 
-}
+// }
 
 class Person implements Greetable {
+  age: number
+
+  constructor(n: number) {
+    this.age = n
+  }
+
+  greet(ja: string) {
+    console.log(ja)
+  }
 }
